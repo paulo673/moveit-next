527b6e9cc6ef672b2f839ec9d18ea68196fc89db
Paulo <Paulo@DESKTOP-FT12GQD>
2021-02-27 09:10:56 -0400

    bug fix

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { CountdownContext } from "../contexts/CountdownContext";
-import styles from "../styles/components/CountDown.module.css";
+import styles from "../styles/components/Countdown.module.css";
 
 export default function Countdown() {
   const {
